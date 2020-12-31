a6b537e46dc6d2053ebcdedc4692feb7fc7dc211
Dohyung Kim <rizz@Dohyungui-MacBookPro.local>
2020-12-31 18:15:05 +0900

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,7 @@
+import java.util.Date as UtilDate
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    var date = UtilDate()
+}
---
+++ src/main/sample/external.kt
@@ -0,0 +1,3 @@
+fun externaldisp(){
+    println("외부 패키지에 작성된 함수")
+}
